8b2fbdccb448bfcd3ea8c593d15a7f7e8515a3d9
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-02-02 23:18:06 -0300

    adicionando página de erro e animacoes

---
+++ src/pages/Error/index.js
@@ -0,0 +1,14 @@
+import "./error.css";
+import "animate.css";
+
+import { Link } from "react-router-dom";
+
+export default function Error() {
+  return (
+    <div className="container-error">
+      <img src="/notfound.png" alt="erro 404 página não encontrada"/>
+      <h1>Página não encontrada</h1>
+      <Link to="/">Voltar pro início</Link>
+    </div>
+  );
+}
